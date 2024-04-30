6aa99fc0aac03f5797eec34fd3d1938c28d25388
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-04-30 10:43:53 +0800

    update

---
+++ oj/leetcode/2701-2800/2798/2798.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int> &hours, int target) {
+        int res = 0;
+        for (int x: hours) {
+            if (x >= target) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
