e710af5dc733d7bc293577f9070c0e9280168405
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-06-10 01:49:33 +0800

    update

---
+++ oj/leetcode/3101-3200/3174/3174.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res;
+        for (char c: s) {
+            if (isdigit(c)) {
+                res.pop_back();
+            } else {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
