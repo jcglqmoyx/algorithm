6e380e3ba49ec1b1f81a1ec8e0082422d0376880
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-06-14 01:29:46 +0800

    update

---
+++ oj/leetcode/1-100/75/75.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int> &nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int i = 0;
+        while (i <= high) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[low]);
+                i++;
+                low++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                swap(nums[i], nums[high]);
+                high--;
+            }
+        }
+    }
+};
