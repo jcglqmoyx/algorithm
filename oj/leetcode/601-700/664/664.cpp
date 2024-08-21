0e679f1bff9a2d1e1209dd5afc03c27b1b57e4e4
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-08-21 23:21:15 +0800

    update

--- oj/leetcode/601-700/664/664.cpp
+++ oj/leetcode/601-700/664/664.cpp
@@ -10,7 +10,6 @@ public:
         for (int i = n - 1; i >= 0; i--) {
             f[i][i] = 1;
             for (int j = i + 1; j < n; j++) {
-                f[i][i] = 1;
                 if (s[i] == s[j]) {
                     f[i][j] = f[i][j - 1];
                 } else {
