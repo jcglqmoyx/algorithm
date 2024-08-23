9d808a584ec2447ef3d5b393249f419854287d8c
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-08-23 16:11:15 +0800

    update

--- oj/leetcode/1801-1900/1847/1847.cpp
+++ oj/leetcode/1801-1900/1847/1847.cpp
@@ -5,31 +5,28 @@ using namespace std;
 class Solution {
 public:
     vector<int> closestRoom(vector<vector<int>> &rooms, vector<vector<int>> &queries) {
-        struct T {
-            int preferred;
-            int min_size;
-            int idx;
-        };
-        auto m = queries.size();
+        auto n = rooms.size(), m = queries.size();
         int idx[m];
         iota(idx, idx + m, 0);
         sort(idx, idx + m, [&](int a, int b) {
             return queries[a][1] < queries[b][1];
         });
-        set<pair<int, int>> id_size, size_id;
+        set<pair<int, int>> id_size;
         for (auto &r: rooms) {
             id_size.emplace(r[0], r[1]);
-            size_id.emplace(r[1], r[0]);
         }
+        sort(rooms.begin(), rooms.end(), [](auto &a, auto &b) {
+            return a[1] < b[1];
+        });
         vector<int> res(m, -1);
-        for (int i = 0; i < m; i++) {
+        for (int u = 0, i = 0; u < rooms.size() && i < m; i++) {
             int id = idx[i];
             int preferred = queries[id][0], min_size = queries[id][1];
-            while (!size_id.empty() && size_id.begin()->first < min_size) {
-                auto it = size_id.begin();
-                id_size.erase(id_size.find({it->second, it->first}));
-                size_id.erase(it);
+            while (u < n && rooms[u][1] < min_size) {
+                id_size.erase({rooms[u][0], rooms[u][1]});
+                u++;
             }
+            if (u == n) break;
             if (!id_size.empty()) {
                 auto it = id_size.lower_bound({preferred, 0});
                 if (it != id_size.end()) {
