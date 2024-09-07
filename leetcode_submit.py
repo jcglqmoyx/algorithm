af2da6c3cb76be7e859568b30c4415da8c6379c8
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-09-07 23:50:45 +0800

    update

--- leetcode_submit.py
+++ leetcode_submit.py
@@ -3,7 +3,7 @@ import time
 import requests
 
 # 1 there must be a '/' in the end of the link!
-url = 'https://leetcode.com/problems/walking-robot-simulation/submit/'
+url = 'https://leetcode.com/problems/linked-list-in-binary-tree/submit/'
 
 profiles = [
     [
@@ -387,72 +387,23 @@ def submit():
         payload = {
             'lang': 'cpp',
             # 2
-            'question_id': '906',
+            'question_id': '1484',
             # 3
             'typed_code': '''
-           class Solution {
-public:
-    static int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_map<int, set<int>> XobSet, YobSet;
-        XobSet.reserve(10000), YobSet.reserve(10000);
-        for(auto& ob: obstacles){
-            int x=ob[0], y=ob[1];
-            XobSet[x].insert(y);
-            YobSet[y].insert(x);
-        }
-
-        const int dir[4][2]={{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-        int x=0, y=0, dx=0, dy=1, face=0, maxD2=0;
-        for(int c: commands){
-            switch(c){
-                case -2: face=(face+1)%4; dx=dir[face][0]; dy=dir[face][1]; break;
-                case -1: face=(face+3)%4; dx=dir[face][0]; dy=dir[face][1]; break;
-                default:
-                    switch(face){
-                        case 0: {//up
-                            auto it=upper_bound(XobSet[x].begin(), XobSet[x].end(), y);
-                            if (it!=XobSet[x].end() && *it<=y+c) y=*it-1;
-                            else y+=c;
-                            break;
-                        }
-                        case 1: {//left
-                            auto it=upper_bound(YobSet[y].rbegin(), YobSet[y].rend(), x, greater<>());
-                            if (it!=YobSet[y].rend() && *it>=x-c) x=*it+1;
-                            else x-=c;
-                            break;
-                        }
-                        case 2: {//down
-                            auto it=upper_bound(XobSet[x].rbegin(), XobSet[x].rend(), y, greater<>());
-                            if (it!=XobSet[x].rend() && *it>=y-c) y=*it+1;
-                            else y-=c;
-                            break;
-                        }
-                        case 3: {//right
-                            auto it=upper_bound(YobSet[y].begin(), YobSet[y].end(), x);
-                            if (it!=YobSet[y].end() && *it<=x+c) x=*it-1;
-                            else x+=c;
-                            break;
-                        }
-                    }
-                    maxD2=max(maxD2, x*x+y*y);
-                }
-        }
-        return maxD2;
-    }
-}; 
                             '''
         }
 
-        response = requests.post(url, json=payload, headers=headers)
+        resp = requests.post(url, json=payload, headers=headers)
 
-        print(profiles[i][2:])
-        print(response.status_code)
-        print(response.json())
-        print()
+        print(
+            'idx: %d,' % i,
+            'info: %s,' % profiles[i][2:],
+            'status: %d,' % resp.status_code,
+            'json: %s.' % resp.json()
+        )
         if i + 1 < len(profiles):
             time.sleep(10)
 
 
 if __name__ == '__main__':
     submit()
-    pass
