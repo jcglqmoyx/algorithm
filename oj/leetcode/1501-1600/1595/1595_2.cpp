972e4b075349d69dcdc6cb8cbdae380e32bdef80
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-10-08 16:58:27 +0800

    update

--- oj/leetcode/1501-1600/1595/1595_2.cpp
+++ oj/leetcode/1501-1600/1595/1595_2.cpp
@@ -9,13 +9,13 @@ public:
         int f[1 << n];
         memset(f, 0x3f, sizeof f);
         f[0] = 0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             int g[1 << m];
             memset(g, 0x3f, sizeof g);
             for (int s = 0; s < 1 << m; s++) {
                 for (int j = 0; j < m; j++) {
-                    g[s | (1 << j)] = min(g[s | (1 << j)], f[s] + cost[i - 1][j]);
-                    g[s | (1 << j)] = min(g[s | (1 << j)], g[s] + cost[i - 1][j]);
+                    g[s | (1 << j)] = min(g[s | (1 << j)], f[s] + cost[i][j]);
+                    g[s | (1 << j)] = min(g[s | (1 << j)], g[s] + cost[i][j]);
                 }
             }
             memcpy(f, g, sizeof g);
