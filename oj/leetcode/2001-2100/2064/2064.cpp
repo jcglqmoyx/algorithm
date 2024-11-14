288aab1be77f9e94c22b252c658e250ecdc3d2ea
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-11-14 17:11:34 +0800

    update

---
+++ oj/leetcode/2001-2100/2064/2064.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int> &quantities) {
+        int l = (int) ((accumulate(quantities.begin(), quantities.end(), 0LL) + n - 1) / n);
+        int r = *max_element(quantities.begin(), quantities.end());
+        auto check = [&](int mid) {
+            int i = 0, j = 0, m = static_cast<int>(quantities.size());
+            for (; j < m; j++) {
+                int x = quantities[j];
+                while (i < n && x) {
+                    x -= min(x, mid);
+                    i++;
+                }
+                if (i == n && x) return false;
+            }
+            return i <= n;
+        };
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (check(mid)) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
